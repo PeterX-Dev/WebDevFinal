0627d2f9c98000f999b92344e3dcf74b797948a5
peterX-Dev <jxiong7@my.bcit.ca>
2020-03-27 13:03:06 -0700

    Change loginPage and add user validity check

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,29 @@
+const mod_user = require('../models/userData');
+const mod_discuss = require('../models/discussionData');
+const mod_post = require('../models/postData');
+
 exports.showLoginPage = function(req,res,next) {  
+    // Display empty login Page
     res.render('loginPage' ,{      
     });
 }
 
 exports.memberLogin = function(req,res,next) {  
-    let replyObj = req.body
-    console.log(replyObj); 
-    res.render('mainPage' ,{      
-    });
+    const loginUser = req.body;
+    let userId;
+
+    if ((userId = mod_user.checkValidity(loginUser)) >= 0) 
+    {
+        // valid member
+        res.render('mainPage' ,{
+        });
+    }
+    else{
+        // inValid user, display error message 
+        res.render('loginPage' ,{InvalidMember: true});
+    }
+
+
 }
 
 exports.newMemberSignUp = function(req,res,next) {
--- models/userData.js
+++ models/userData.js
@@ -14,7 +14,37 @@
 //     LikesNo(Number) 
 // }
 
-var userList;
+// This is test data, will be replaced by DB API later
+var userList = [
+    {
+        "Id":0,
+        "FirstName":"user0",
+        "LastName":"Xiong",
+        "Email":"guilin2000@yahoo.com",
+        "Password":"12",
+        "Description":"This is a test!!!", 
+        "Country":"12",
+        "DOB":"",
+        "ImageUrl":"https://randomuser.me/api/portraits/med/men/22.jpg",
+        "PostNo":0,
+        "MsgNo":0,
+        "LikesNo":0
+        }, 
+    {
+        "Id":1,
+        "FirstName":"user1",
+        "LastName":"Xiong",
+        "Email":"guilin2000@yahoo.com",
+        "Password":"123",
+        "Description":"This is a test!!!", 
+        "Country":"12",
+        "DOB":"",
+        "ImageUrl":"https://randomuser.me/api/portraits/med/women/22.jpg",
+        "PostNo":0,
+        "MsgNo":0,
+        "LikesNo":0
+    }
+];
 
 function addUser(e) {
 
@@ -32,9 +62,23 @@ function getUser(id) {
     return userList[id];
 }
 
+function checkMemberValidity(user) {
+    var member = userList.filter(x => x.Email === user.email && x.Password === user.password);
+
+    if (member === undefined || member.length == 0) {
+        // array empty or does not exist
+        return -1;
+    }
+    else{
+        return member[0].Id;
+    }
+    
+}
+
 module.exports = {
     add : addUser,
     update : updateUser,
     getall : getAllUsers,
-    getByid : getUser
+    getByid : getUser,
+    checkValidity: checkMemberValidity
 }
