4ba89a9b189c8f35c8c78751babc645a040e346c
peterX-Dev <jxiong7@my.bcit.ca>
2020-03-27 18:18:18 -0700

    use session to display user profile in profileEdit Page

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -5,8 +5,11 @@ exports.showProfilePage = function(req,res,next) {
     
     // TBD-Peter: Need to get profile infomation of current user and display 
     // them in profile edit page. 
-    
-    res.render('profileEditPage' ,{      
+    let userObj = mod_user.getByid(req.session.userId);
+    console.log(userObj);
+   
+    res.render('profileEditPage' ,{
+        user: userObj   
     });
 }
 
