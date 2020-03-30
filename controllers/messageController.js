44eba4187e9aef11d421f857d5168dd48b138895
ellainec <chan.ellaine@gmail.com>
2020-03-30 01:46:12 -0700

    message page styled with mock data

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,13 +1,18 @@
 exports.showMessagePage = function(req,res,next) {  
-    res.render('messagePage' ,{      
-    });
+    let mockMessage = [{name: "Frank Lee", title: "hi there", senderImg: "https://randomuser.me/api/portraits/med/women/2.jpg", date:"Sep 12"},
+    {name: "Frank Lee", title: "hi there", senderImg: "https://randomuser.me/api/portraits/med/women/2.jpg", date:"Sep 12"}
+    ];
+    let mockReply = [
+        {sender: "Frank Lee", date: "SEP 4", time: "9:57 PM", message: "Umm, give me some time to think about it ...", senderImg: "https://randomuser.me/api/portraits/med/men/24.jpg"},
+        {sender: "Jane Way", date: "SEP 4", time: "9:57 PM", message: "Umm, give me some time to think about it ...", senderImg: "https://randomuser.me/api/portraits/med/women/2.jpg"}
+    ];
+    res.render('messagePage',{ messagePageCSS: true, message: mockMessage, reply: mockReply});
 }
 
 exports.showMessageEditPage = function(req,res,next) {  
     let replyObj = req.body
     console.log(replyObj); 
-    res.render('messageEditPage' ,{      
-    });
+    res.render('messageEditPage' ,{      });
 }
 
 exports.sendMessage = function(req,res,next) {
