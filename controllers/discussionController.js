ba7115c87ae597dbdb0611d409cf3037d7c48138
Patrick <pkhwong.92@gmail.com>
2020-03-30 12:35:00 -0700

    Updated /discussion route

--- controllers/discussionController.js
+++ controllers/discussionController.js
@@ -1,10 +1,10 @@
 let db = require('../DB/db');
+let mod_user = require('../models/discussionData.js');
 
 exports.showDiscussionPage = function(req,res,next) {  
-    db.query("Insert into Post (content) VALUES ('second time!')");
-    console.log("added to db");
-    res.render('discussionPage' ,{      
-    });
+    // db.query("Insert into Post (content) VALUES ('second time!')");
+    // console.log("added to db");
+    res.render('discussionPage' ,{discussion: mod_user.getall, discussionCSS: true});
 }
 
 exports.showReplyPage = function(req,res,next) {  
--- models/discussionData.js
+++ models/discussionData.js
@@ -5,7 +5,26 @@
 //     Topic(String)      
 // }
 
-var discussionList;
+var discussionList = [
+    {
+        "Id": "1",
+        "ImageUrl": "https://randomuser.me/api/portraits/women/26.jpg",
+        "Subject": "nodejs",
+        "Topic": "Help with Nodejs!!!",
+        "Message": "I am a noob. I only know html and css. Pls halp!",
+        "Date": "30 mar 2020",
+        "Replies": "2"
+    },
+    {
+        "Id": "2",
+        "ImageUrl": "https://randomuser.me/api/portraits/men/26.jpg",
+        "Subject": "php",
+        "Topic": "Jesus Christ, I hate this framework.",
+        "Message": "Don't mind me. I am venting. jk lol",
+        "Date": "1 apr 2020",
+        "Replies": "10"
+    }
+];
 
 function addDiscussion(e) {
 
