656c1acaf61e855ce657f6acd2046415d2ff00ef
peterX-Dev <jxiong7@my.bcit.ca>
2020-03-30 18:31:24 -0700

    Add post bar

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,27 @@
 exports.showMainPage = function(req,res,next) {
     let name = req.session.userId;
     console.log("userid: "+name);
-    res.render('mainPage' ,{      
+ 
+    let postList = [
+        {
+            ImgUrl: "https://randomuser.me/api/portraits/med/men/22.jpg",
+            Subject:"Hello1",
+            Topic: "php",
+            PostContent: "This is a test0 This is a test0 This is a test0 This is a test0 This is a test0",
+            PostDate: "Oct 10 2019",
+            Replies: 5
+        },
+        {
+            ImgUrl: "https://randomuser.me/api/portraits/med/women/22.jpg",
+            Subject:"Hello2",
+            Topic: "node",
+            PostContent: "This is a test1 This is a test1 This is a test1 This is a test1 This is a test1",
+            PostDate: "Oct 11 2019",
+            Replies: 1
+        }
+    ];
+    res.render('mainPage' ,{
+        posts: postList
     });
 }
 
