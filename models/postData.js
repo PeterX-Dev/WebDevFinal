35c29ecfb2d2f55a4f4f2633e3256f135e2a4008
peterX-Dev <jxiong7@my.bcit.ca>
2020-04-03 01:39:57 -0700

    Delete comment lines

--- models/postData.js
+++ models/postData.js
@@ -5,35 +5,7 @@
 //     Topic(String)      
 // }
 let db = require('../DB/db');
-// var postList = [
-//     {
-//         "id": "1",
-//         "ImageUrl": "https://randomuser.me/api/portraits/women/26.jpg",
-//         "Subject": "nodejs",
-//         "Topic": "Help with Nodejs!!!",
-//         "Message": "I am a noob. I only know html and css. Pls halp!",
-//         "Date": "30 mar 2020",
-//         "Replies": "2"
-//     },
-//     {
-//         "id": "2",
-//         "ImageUrl": "https://randomuser.me/api/portraits/men/26.jpg",
-//         "Subject": "php",
-//         "Topic": "I hate this framework.",
-//         "Message": "Don't mind me. I am venting. jk lol",
-//         "Date": "1 apr 2020",
-//         "Replies": "10"
-//     },
-//     {
-//         "id": "3",
-//         "ImageUrl": "https://randomuser.me/api/portraits/men/60.jpg",
-//         "Subject": "nodejs",
-//         "Topic": "Need HELP!",
-//         "Message": "How do I make a server using nodejs?",
-//         "Date": "1 apr 2020",
-//         "Replies": "5"
-//     }
-// ];
+
 let topicList = [];
 let postList = [];
 
