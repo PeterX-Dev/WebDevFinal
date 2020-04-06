960abb831fe029c96ba1321ee9f0cde13c60e34e
ellainec <chan.ellaine@gmail.com>
2020-04-05 17:25:39 -0700

    fix spelling

--- DB/db.js
+++ DB/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({  
     host: 'ec2-18-235-20-228.compute-1.amazonaws.com',  
     user: 'bjtadqbtjojevm',  
     database: 'd1vli1k0tgfhb6',  
-    password: `${process.env.DATABASE_PASSOWRD}`,
+    password: `${process.env.DATABSE_PASSWORD}`,
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
