1d11a154ed2f9a6e88a892e08c0e82464f18b710
ellainec <chan.ellaine@gmail.com>
2020-04-05 17:23:46 -0700

    test

--- DB/db.js
+++ DB/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({  
     host: 'ec2-18-235-20-228.compute-1.amazonaws.com',  
     user: 'bjtadqbtjojevm',  
     database: 'd1vli1k0tgfhb6',  
-    password: '81aa6a0e96db3eefd2b369c76003cb136c97bd66fa1a8e03d97bf936a2241a57',
+    password: `${process.env.DATABASE_PASSOWRD}`,
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
