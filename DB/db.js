dfb3ba07d500531223cc26f42ead23ff7fcc2540
ellainec <chan.ellaine@gmail.com>
2020-04-07 14:45:59 -0700

    update

--- DB/db.js
+++ DB/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({  
-    host: 'ec2-18-235-20-228.compute-1.amazonaws.com',  
-    user: 'bjtadqbtjojevm',  
-    database: 'd1vli1k0tgfhb6',  
-    password: `${process.env.DATABSE_PASSWORD}`, 
+    host: process.env.DATABASE_HOST,  
+    user: process.env.DATABASE_USER,  
+    database: process.env.DATABASE,  
+    password: process.env.DATABSE_PASSWORD, 
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
